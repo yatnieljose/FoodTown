0027d0d25b9e0e80faf85694b8a31e1787090d94
Yatniel Ramos <yatnieljose0208@gmail.com>
2023-05-03 02:15:19 +0200

    new file:   src/Menu/MenuFactory/AmericanMenuFactory.java 	.vscode/ 	bin/ 	src/FoodTown.java 	src/Menu/AbsMenu.java 	src/Menu/MenuFactory/MenuFactoryIF.java 	src/Menu/MenuIF.java 	src/Menu/SubMenu/

---
+++ src/Menu/MenuFactory/AmericanMenuFactory.java
@@ -0,0 +1,29 @@
+package Menu.MenuFactory;
+
+import Menu.MenuIF;
+
+public class AmericanMenuFactory implements MenuFactoryIF
+{
+
+    @Override
+    public MenuIF createLunchMenu() 
+    {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createLunchMenu'");
+    }
+
+    @Override
+    public MenuIF createDinnerMenu() 
+    {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createDinnerMenu'");
+    }
+
+    @Override
+    public MenuIF createDessertMenu() 
+    {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createDessertMenu'");
+    }
+
+}
